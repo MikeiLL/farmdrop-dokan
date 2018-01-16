0e21e12af1a3df0b6a157009170b934bd964fd68
Mike iLL Kilmer <mike@mzoo.org>
2018-01-16 09:05:43 -0600

    Add a couple of helper functions.

--- functions.php
+++ functions.php
@@ -26,6 +26,43 @@ function kill($data) {
 	die(var_dump($data));
 }
 
+if ( ! function_exists( 'mZ_write_to_file' ) ) {
+	/**
+	 * Write message out to file
+	 * @param  String/Array  $message		What we want to examine
+	 * @param  String $file_path		A valid file path, default: file in WP_CONTENT_DIR
+	 */
+
+	function mZ_write_to_file($message, $file_path='')
+	{
+			$file_path = ( ($file_path == '') || !file_exists($file_path) ) ? WP_CONTENT_DIR . '/mbo_debug_log.txt' : $file_path;
+			$header = date('l dS \o\f F Y h:i:s A', strtotime("now")) . " \nMessage:\t ";
+
+			if (is_array($message)) {
+					$header = "\nMessage is array.\n";
+					$message = print_r($message, true);
+			}
+			$message .= "\n";
+			file_put_contents(
+					$file_path,
+					$header . $message,
+					FILE_APPEND | LOCK_EX
+			);
+	}
+}
+
+if ( ! function_exists( 'mz_pr' ) ) {
+	/**
+	 * Write message out to file
+	 * @param  String/Array  $message		What we want to examine in browser
+	 */
+	function mz_pr($message) {
+		echo "<pre>";
+		print_r($message);
+		echo "</pre>";
+	}
+}
+
 function my_theme_enqueue_styles() {
 	$parent_style = 'parent-style';
 	// This is 'twentyfifteen-style' for the Twenty Fifteen theme.
